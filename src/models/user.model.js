7504372032fe642cac8db6a616449f6f904d6a0e
Diego Garcia <tu_correo_de_github@example.com>
2025-09-19 16:30:54 -0300

    Se agregó más atributos en el modelo 'USERS'

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,25 +1,57 @@
 //model user
 import { model, Schema } from "mongoose";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    minlength: 3,
-    maxlenght: 20,
-  },
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      minlength: 3,
+      maxlenght: 20,
+    },
 
-  email: {
-    type: String,
-    rquired: true,
+    email: {
+      type: String,
+      rquired: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    profile: {
+      firstName: {
+        type: String,
+        required: true,
+        minlenght: 2,
+        maxlenght: 50,
+      },
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minlenght: 2,
+      maxlenght: 50,
+    },
+    biography:{
+        type: String,
+        maxlenght: 500
+    },
+    avatarUrl: {
+        type: String,
+    },
+    birthDate: {
+        type: Date
+    }
   },
-  password: {
-    type: String,
-    required: true,
-  },
-},{
-    timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const UserModel = model("Users", userSchema);
