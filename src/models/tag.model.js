98ab224a4f132e6ef912a4e609a18fe4d07d6eba
Diego Garcia <tu_correo_de_github@example.com>
2025-09-19 16:22:14 -0300

    Se creó el modelo de 'TAGS'

--- src/models/tag.model.js
+++ src/models/tag.model.js
@@ -1,1 +1,23 @@
 //tag model
+import { model, Schema } from "mongoose";
+
+const tagSchema = new Schema(
+  {
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        milenght: 2,
+        maxlenght: 30,
+    },
+    description: {
+        type: String,
+        maxlenght: 200
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+export const TagsModel = model("Tags", tagSchema)
