9c00f8e5d3f5ddd5ec28fb694e5d1c4f0a9eec24
Diego Garcia <tu_correo_de_github@example.com>
2025-09-19 15:37:03 -0300

    se realizó la configuración del servidor y la base de datos

---
+++ app.js
@@ -0,0 +1,29 @@
+import "dotenv/config"
+import express from "express";
+import cors from "cors"
+// import { routes } from "./src/routes/indexRouter.js"
+import { connectDB } from "./src/config/database.js"
+
+const PORT = process.env.PORT
+const app = express();
+
+//middlewares para la configuración del servidor
+app.use(express.json())
+app.use(cors({
+    // origin: 
+}))
+
+
+//middleware de rutas
+// app.use("/api", routes)
+
+//test conection BD
+connectDB()
+.then(() => {
+ app.listen(PORT, () => {
+   console.log("Server listening on port", PORT);
+  });
+})
+ .catch((error) => {
+ console.error("Error connecting to dataBase:", error);
+});
--- src/config/database.js
+++ src/config/database.js
@@ -1,1 +1,14 @@
 //database
+import mongoose from "mongoose";
+const MONGODB_URI = process.env.MONGODB_URI
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    // para borrar toda la base de datos
+    await mongoose.connection.dropDatabase();
+    console.log("Conectado a la base de datos");
+  } catch (error) {
+    console.log("No se pudo conectar a la base de datos", error);
+  }
+};
