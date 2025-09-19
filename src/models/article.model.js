09b8cba7b726206bfccbe13026a5dae8d41f7642
Diego Garcia <tu_correo_de_github@example.com>
2025-09-19 16:16:43 -0300

    Se creó el modelo de 'ARTICLES'

--- src/models/article.model.js
+++ src/models/article.model.js
@@ -1,1 +1,44 @@
 //article model
+import { model, Schema, Types } from "mongoose";
+
+const Schema = new Schema(
+  {
+    title: {
+      type: String,
+      unique: true,
+      required: true,
+      minlenght: 3,
+      maxlenght: 200,
+    },
+    content: {
+      type: String,
+      reuired: true,
+      minlenght: 50,
+    },
+    excerpt: {
+      type: String,
+      maxlenght: 500,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ["published", "archived"],
+      default: "published",
+    },
+    author: {
+      type: Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+    tags: [
+      {
+        type: Types.ObjectId,
+        ref: "tags",
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true
+  }
+);
