56b4c385a57f177789794194f37a9c4d472919ed
Diego Garcia <tu_correo_de_github@example.com>
2025-09-19 16:41:54 -0300

    Se creó el modelo de 'COMMENTS'

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,1 +1,30 @@
 //comment model
+import { model, Schema, Types } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      minlenght: 5,
+      maxlenght: 500,
+    },
+    author: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    article: [
+      {
+        type: Types.ObjectId,
+        ref: "Articles",
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const CommentModel = model("Comments", commentSchema)
