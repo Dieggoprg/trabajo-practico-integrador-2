75268f970ecc6cbda8fe1f0633ab2bc758894237
Diego Garcia <tu_correo_de_github@example.com>
2025-09-19 15:57:43 -0300

    Se creó el modelo de 'USERS'

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,1 +1,23 @@
 //model user
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3,
+    maxlenght: 20,
+  },
+
+  email: {
+    type: String,
+    rquired: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export const UserModel = model("Users", userSchema);
