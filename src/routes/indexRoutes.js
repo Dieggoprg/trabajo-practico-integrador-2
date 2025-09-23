5738c388c49eef59308e8bcd4797ce78cd4fe84e
Diego Garcia <tu_correo_de_github@example.com>
2025-09-23 15:50:40 -0300

    Archivo barril de rutas actualizado

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -4,9 +4,11 @@ import { routeUser } from "./user.route.js";
 import { routeTag } from "./tag.route.js";
 import { routeComment } from "./comment.route.js";
 import { routeArticle } from "./article.route.js";
+import { authRoutes } from "./auth.route.js";
 
-export const routes = Router()
+export const routes = Router();
 
+routes.use (authRoutes)
 routes.use(routeUser)
 routes.use(routeTag)
 routes.use(routeComment)
