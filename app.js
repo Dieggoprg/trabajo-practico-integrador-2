d958ad140b9c476e4ef5430d9386e1bbb53b7e6c
Diego Garcia <tu_correo_de_github@example.com>
2025-09-23 15:55:07 -0300

    se agregaron otras configuraciones al servidor

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-import "dotenv/config"
+//IMPORTACIONES
+import "dotenv/config";
 import express from "express";
-import cors from "cors"
-import { routes } from "./src/routes/indexRoutes.js"
-import { connectDB } from "./src/config/database.js"
+import cors from "cors";
+import { routes } from "./src/routes/indexRoutes.js";
+import { connectDB } from "./src/config/database.js";
+import cookieParser from "cookie-parser";
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const app = express();
 
 //middlewares para la configuración del servidor
-app.use(express.json())
-app.use(cors({
-    // origin: 
-}))
-
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:", PORT,
+    credentials: true, // CRUCIAL: permitir cookies
+  })
+);
+app.use(cookieParser()); // NECESARIO: para leer req.cookies
 
 //middleware de rutas
-app.use("/api", routes)
+app.use("/api", routes);
 
 //test conection BD
 connectDB()
-.then(() => {
- app.listen(PORT, () => {
-   console.log("Server listening on port", PORT);
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Server listening on port", PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to dataBase:", error);
   });
-})
- .catch((error) => {
- console.error("Error connecting to dataBase:", error);
-});
