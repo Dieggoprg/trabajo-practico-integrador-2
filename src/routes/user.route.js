eff85245e940967549332a9adfddf564d36d9c9f
Diego Garcia <tu_correo_de_github@example.com>
2025-09-23 15:52:12 -0300

    se realizaron modificaciones en el archivo de las routes de User

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,9 @@
 //user route
 import { Router } from "express";
-import { getAllUsers, createUsers, getByIdUser, updateUsers, deleteUser } from "../controllers/user.controller.js";
+import { getAllUsers, getByIdUser, updateUsers, deleteUser } from "../controllers/user.controller.js";
 
 export const routeUser = Router();
 
-routeUser.post("/users", createUsers)
 routeUser.get("/users", getAllUsers)
 routeUser.get("/user/:id", getByIdUser)
 routeUser.put("/users/:id", updateUsers)
